3648138445042330ed61d6f37ad80cf4df3b5bec
vamsiampolu <vamsideepak03@gmail.com>
2015-04-22 02:20:46 +0530

    Wrote a directive to create a todo

--- app/app.js
+++ app/app.js
@@ -24,11 +24,42 @@ app.factory("TodoList",function(){
 			btnText:'Done',
 			description:"Ensure that browser-sync is working correctly",
 			done:false
+		},
+		{
+			id:2,
+			task:"Create a service",
+			btnText:'Done',
+			description:'Build a service with CouchDB as the backend and PouchDB on the frontend'
+		},{
+			id:3,
+			task:'Create a directive using an isolate scope for each todo',
+			description:'Each Todo is a unique UI component with a specialized card layout,I might as well put it in a directive',
+			btnText:'Done'
 		}];
 
 		return todos;	
 	};
 	return instance;
+});
+
+app.directive("todoItem",function(){
+	var dirDefObj = {
+		restrict:'E',
+		templateUrl:'app/templates/todo.html',
+		scope:{
+			todo:'=value'
+		},
+		controller:function($scope)
+		{
+			$scope.clickDone = function clickDone(){
+				//two tasks (1)toggle the done value on the todo (2) toggle the btnText on the todo
+				$scope.todo.done = !$scope.todo.done;
+				$scope.todo.btnText = $scope.todo.done?'Reinstate':'Done';
+			};
+		},
+		replace:true
+	};
+	return dirDefObj;
 });
 
 app.controller("TodoCtrl",["$scope","TodoList",function($scope,TodoList){
