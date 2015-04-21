06b3ca74b021bc9ebf6e17c33bf1616c797bf396
vamsiampolu <vamsideepak03@gmail.com>
2015-04-21 21:01:31 +0530

    Moved data to a factory method

--- app/app.js
+++ app/app.js
@@ -8,24 +8,38 @@ app.controller("HelloCtrl",function($scope){
 	$scope.categories = ["Sass","Less","Stylus","CSS"];
 });
 
-app.controller("TodoCtrl",function($scope){
-	$scope.todos = [{
-		id:0,
-		task:"Create a controller",
-		description:"Create a controller for todos.",
-		done:false
-	},
-	{
-		id:1,
-		task:"Reload set task",
-		description:"Ensure that browser-sync is working correctly",
-		done:false
-	}];
-	$scope.clickDone = function clickDone(id){
-		console.log("Inside click done");
-		console.log($scope.todos[id]);
-		$scope.todos[id].done = !$scope.todos[id].done;
+app.factory("TodoList",function(){
+	var instance = {};
+	instance.get = function get(){
+		var todos = [{
+			id:0,
+			btnText:'Done',
+			task:"Create a controller",
+			description:"Create a controller for todos.",
+			done:false
+		},
+		{
+			id:1,
+			task:"Reload set task",
+			btnText:'Done',
+			description:"Ensure that browser-sync is working correctly",
+			done:false
+		}];
+
+		return todos;	
 	};
+	return instance;
 });
+
+app.controller("TodoCtrl",["$scope","TodoList",function($scope,TodoList){
+	$scope.todos = TodoList.get();
+	$scope.clickDone = function clickDone(id){
+		$scope.todos[id].done = !$scope.todos[id].done;
+		if($scope.todos[id].done)
+			$scope.todos[id].btnText = 'Reinstate';
+		else
+			$scope.todos[id].btnText = 'Done';
+	};
+}]);
 
 module.exports = app;
