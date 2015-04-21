8cabad9cb7848b9afbab85917cf6ec759f92aac8
vamsiampolu <vamsideepak03@gmail.com>
2015-04-21 05:46:22 +0530

    Stuck trying to do auto-strike through

--- app/app.js
+++ app/app.js
@@ -10,12 +10,21 @@ app.controller("HelloCtrl",function($scope){
 
 app.controller("TodoCtrl",function($scope){
 	$scope.todos = [{
+		id:0,
 		task:"Create a controller",
 		done:false
-	},{
+	},
+	{
+		id:1,
 		task:"Reload set task",
 		done:false
 	}];
+	$scope.clickDone = function clickDone(id){
+		console.log("Inside click done");
+		console.log($scope.todos[id]);
+		var todo = $scope.todos[id];
+		todo.done = !todo.done;
+	};
 });
 
 module.exports = app;
